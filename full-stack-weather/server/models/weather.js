d0827548fa35c1e979bf9c209da5dd7e3d594c96
Kush Panjabi <kushpanjabi@icloud.com>
2020-05-29 12:09:06 -0700

    Added backend

---
+++ full-stack-weather/server/models/weather.js
@@ -0,0 +1,19 @@
+const request = require('request-promise');
+
+const API_KEY = "a21eef252bd04fb6bbf53eb4978dab01";
+
+class Weather {
+  static retrieveByCity (city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+      json: true
+    }).then((res) => {
+      callback(res);
+    }).catch((err) => {
+      console.log(err);
+      callback({ error: 'Could not reach OpenWeatherMap API.' });
+    });
+  }
+}
+
+module.exports = Weather;
